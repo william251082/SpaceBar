774781888ed548b3e933f4c8b47a38723350cdf8
William del Rosario <williamdelrosario@yahoo.com>
2018-02-12 20:07:13 +0100

    Sprintf, routes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,13 +8,28 @@
 
 namespace App\Controller;
 
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
     public function  homepage()
     {
         return new Response('New Page');
     }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function  show($slug)
+    {
+        return new Response(Sprintf('Future page show the article: %s',
+                                    $slug
+        ));
+    }
+
+
 }
