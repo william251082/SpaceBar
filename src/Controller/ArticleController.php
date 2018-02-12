e2090b95d53c8211c23d80dd5af13945bf7a4c07
William del Rosario <williamdelrosario@yahoo.com>
2018-02-12 22:42:30 +0100

    Ajax request for heart likes.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -16,15 +16,15 @@ use Symfony\Component\HttpFoundation\Response;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function  homepage()
     {
-        return new Response('New Page');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
