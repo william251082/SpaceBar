53fcb994ed8225d57b20033715b3fccbfc11b251
William del Rosario <williamdelrosario@yahoo.com>
2018-02-17 13:41:38 +0100

    Config parameters

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: williamdelrosario
- * Date: 13/02/2018
- * Time: 17:03
- */
 
 namespace App\Service;
 
@@ -17,27 +11,26 @@ class MarkdownHelper
     private $cache;
     private $markdown;
     private $logger;
-//    private $isDebug;
-//
-        // First 2 arguments in the constructor are autowired
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
+    private $isDebug;
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
         $this->logger = $markdownLogger;
-//        $this->isDebug = $isDebug;
+        $this->isDebug = $isDebug;
     }
 
     public function parse(string $source): string
     {
-        if (stripos($source, 'Doutz') !== false) {
-            $this->logger->info('Hi Doutz!');
+        if (stripos($source, 'bacon') !== false) {
+            $this->logger->info('They are talking about bacon again!');
         }
-//
-//        // skip caching entirely in debug
-//        if ($this->isDebug) {
-//            return $this->markdown->transform($source);
-//        }
+
+        // skip caching entirely in debug
+        if ($this->isDebug) {
+            return $this->markdown->transform($source);
+        }
 
         $item = $this->cache->getItem('markdown_'.md5($source));
         if (!$item->isHit()) {
